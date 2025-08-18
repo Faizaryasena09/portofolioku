ebede5718dfedba961732d92ae7fc8a56226af14
Faizaryasena09 <cenax09@gmail.com>
2025-08-18 16:34:29 +0700

    addimg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,42 @@
-import { Component, Directive, ElementRef, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  galleries = [
+    {
+      title: 'Galeri',
+      photos: [
+        'https://imgur.com/fc3mYxP.jpg',
+        'https://imgur.com/wBKAkPZ.jpg',
+        'https://imgur.com/ra3wS0Z.jpg',
+        'https://imgur.com/l9bGiOX.jpg',
+        'https://imgur.com/8OS5Dwg.jpg',
+        'https://imgur.com/8OS5Dwg.jpg',
+        'https://imgur.com/Xr7Mzav.jpg',
+        'https://imgur.com/0q7H1zB.jpg',
+        'https://imgur.com/P38eyA7.jpg',
+        'https://imgur.com/dtq9fya.jpg',
+        'https://imgur.com/wfop4XG.jpg',
+        'https://imgur.com/ZqMkeiK.jpg',
+        'https://imgur.com/biAXpSX.jpg',
+        'https://imgur.com/TzG4Eww.jpg',
+        'https://imgur.com/qCSvfUK.jpg'
+      ]
+    },
+    {
+      title: 'Desain grafis',
+      photos: [
+        'https://imgur.com/w03A7OL.jpg',
+        'https://imgur.com/NYa8lHV.jpg',
+        'https://imgur.com/9gxscr7.jpg'
+      ]
+    },
+  ];
 }
